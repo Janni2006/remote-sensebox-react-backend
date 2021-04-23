5c3d8aa1b0d8b3001614054668cffb3133f50a88
Jan-Niklas Wielpuetz <janni.wielpuetz@gmail.com>
2021-04-23 16:16:04 +0200

    Standalone mode of main

--- server/index.js
+++ server/index.js
@@ -23,9 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/api", apiRouter);
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
-// });
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 startJobs();
 
