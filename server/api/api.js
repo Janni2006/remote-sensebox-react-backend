ee15a6260951c46b6c3c4b5026714952516f2933
Jan-Niklas Wielpuetz <janni.wielpuetz@gmail.com>
2021-05-11 22:28:59 +0200

    Updated queue to use websockets

--- server/api/api.js
+++ server/api/api.js
@@ -4,9 +4,13 @@ const apiRouter = express.Router();
 const uploadRouter = require('./upload');
 const queueRouter = require('./queue');
 const sketchRouter = require('./sketches');
+const workshopRouter = require('./workshop');
+const camRouter = require('./cam');
 
 apiRouter.use("/", uploadRouter);
 apiRouter.use("/", queueRouter);
 apiRouter.use("/", sketchRouter);
+apiRouter.use("/", workshopRouter);
+apiRouter.use("/", camRouter);
 
 module.exports = apiRouter;
--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const http = require("http");
+const socketIo = require("socket.io");
 const cookieParser = require('cookie-parser');
 const app = express();
 const path = require('path');
@@ -6,13 +8,24 @@ const cors = require('cors');
 const startJobs = require('./handlers/startHandler');
 const apiRouter = require('./api/api');
 const jsonServer = require('json-server');
-const server = jsonServer.create();
+const jsonserver = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
+const crypto = require("crypto");
+
+const update = require('./websockets/updateQueue')
+const uuid = require('./uuid');
 
 require('dotenv/config')
 
+const randomId = () => { return crypto.randomBytes(8).toString("hex") };
+
+const { InMemorySessionStore } = require("./websockets/sessionStore");
+const sessionStore = new InMemorySessionStore();
+
 global.__basedir = __dirname.split("/").splice(0, __dirname.split("/").length - 1).join("/")
+global[uuid] = {}
+
 
 
 app.use(cors({ credentials: true }));
@@ -23,19 +36,72 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/api", apiRouter);
 
+const server = http.createServer(app);
+io = socketIo(server, {
+  cors: {
+    origin: "http://192.168.1.134:3000",
+  },
+});
+
+io.use((socket, next) => {
+  const sessionID = socket.handshake.auth.sessionID;
+  if (sessionID) {
+    const session = sessionStore.findSession(sessionID);
+    if (session) {
+      socket.sessionID = sessionID;
+      return next();
+    }
+  }
+  socket.sessionID = randomId();
+  next();
+});
+
+io.on('connection', (socket) => {
+  // persist session
+  sessionStore.saveSession(socket.sessionID, {
+    userID: socket.userID,
+    username: socket.username,
+    connected: true,
+  });
+
+  // emit session details
+  socket.emit("session", {
+    sessionID: socket.sessionID,
+    userID: socket.userID,
+  });
+
+  // join the "userID" room
+  socket.join(socket.userID);
+
+  // notify users upon disconnection
+  socket.on("disconnect", async () => {
+    const matchingSockets = await io.in(socket.userID).allSockets();
+    const isDisconnected = matchingSockets.size === 0;
+    if (isDisconnected) {
+      // update the connection status of the session
+      sessionStore.saveSession(socket.sessionID, {
+        connected: false,
+      });
+    }
+  });
+  update();
+});
+
+global[uuid].io = io;
+
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 startJobs();
 
-app.listen(process.env.PORT || 4000, '0.0.0.0',
+server.listen(process.env.PORT || 4000, '0.0.0.0',
   () => console.log(`Example app listening at http://localhost:${process.env.PORT || 4000}`)
 );
 
 //start json-server
-server.use(middlewares);
-server.use(router);
-server.listen(4500, '127.0.0.1', () => {
+jsonserver.use(middlewares);
+jsonserver.use(router);
+jsonserver.listen(4500, '127.0.0.1', () => {
   console.log("JSON Server is running")
 })
